86c82b31a2479512bd7dd0024e484f1498d13375
Victoria Olivieri <70556187+vickyrose456@users.noreply.github.com>
2022-02-06 00:38:37 -0500

    Updates to json Response.js

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,86 +1,73 @@
 const users = {};
 
 const respondJSON = (request, response, status, object) => {
-  //object for our headers
-  //Content-Type for json
+  // object for our headers
+  // Content-Type for json
   const headers = {
     'Content-Type': 'application/json',
   };
-  
-  //send response with json object
+
+  // send response with json object
   response.writeHead(status, headers);
   response.write(JSON.stringify(object));
   response.end();
 };
 
-//function to respond without json body
-//takes request, response and status code
+// function to respond without json body
+// takes request, response and status code
 const respondJSONMeta = (request, response, status) => {
-  //object for our headers
-  //Content-Type for json
+  // object for our headers
+  // Content-Type for json
   const headers = {
     'Content-Type': 'application/json',
   };
 
-  //send response without json object, just headers
+  // send response without json object, just headers
   response.writeHead(status, headers);
   response.end();
 };
 
-// get user object
-// should calculate a 200
 const getUsers = (request, response) => {
-  //json object to send
+  // json object to send
   const responseJSON = {
     users,
   };
 
-  //return 200 with message
   return respondJSON(request, response, 200, responseJSON);
 };
 
-// get meta info about user object
-// should calculate a 200 
-const getUsersMeta = (request, response) => {
-  //return 200 without message, just the meta data
-  return respondJSONMeta(request, response, 200);
-};
+const getUsersMeta = (request, response) => respondJSONMeta(request, response, 200);
 
-//function just to update our object 
 const updateUser = (request, response) => {
-  //change to make to user
-  //This is just a dummy object for example
   const newUser = {
     createdAt: Date.now(),
   };
 
-  // modifying our dummy object
-  // just indexing by time for now
   users[newUser.createdAt] = newUser;
 
-  //return a 201 created status
+  // return a 201 created status
   return respondJSON(request, response, 201, newUser);
 };
 
 // function for 404 not found requests with message
 const notFound = (request, response) => {
-  //create error message for response
+  // create error message for response
   const responseJSON = {
     message: 'The page you are looking for was not found.',
     id: 'notFound',
   };
 
-  //return a 404 with an error message
+  // return a 404 with an error message
   respondJSON(request, response, 404, responseJSON);
 };
 
 // function for 404 not found without message
 const notFoundMeta = (request, response) => {
-  //return a 404 without an error message
+  // return a 404 without an error message
   respondJSONMeta(request, response, 404);
 };
 
-//set public modules
+// set public modules
 module.exports = {
   getUsers,
   getUsersMeta,
